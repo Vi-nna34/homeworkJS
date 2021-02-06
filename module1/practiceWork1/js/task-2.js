43ba977b733567703e367d7ccfe9060d8e1c5cd6
Vi-nna34 <joannaang063@gmail.com>
2021-02-07 00:01:08 +0300

    added PractiseWork

---
+++ module1/practiceWork1/js/task-2.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//const
+const hotel = 'Victory';
+
+const isLightOn = true;
+
+const user = null;
+
+const message = 'Welcome!';
+
+const quantity = 25;
+
+//let
+let age = 15;
+age = 40;
+
+let discountPrice = 250;
+discountPrice = 1130;
+
+let fastfood;
+fastfood = 'Popcorn';
+
+let color = 'green';
+
+let isUserLoggedIn = false;
+isUserLoggedIn = true;
